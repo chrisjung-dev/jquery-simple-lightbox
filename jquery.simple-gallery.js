a4a203639d4fe4aa27bc3d7c3827effa2611241b
Chris Jung <campino2k@gmail.com>
2010-07-12 21:59:36 +0200

    Added demo html
    - Added demo html
    - added jQuery-Startup

--- jquery.simple-gallery.js
+++ jquery.simple-gallery.js
@@ -2,45 +2,46 @@
  * jquery.simple-gallery
  * CC-BY-NC-SA Chris Jung <campino2k@gmail.com>
  */
-
-$("a[href$='png']:has(img), a[href$='jpg']:has(img), a[href$='gif']:has(img)").click(
-	function(e) {
-		$this = $(this);
-		var pic = $('<img />', {
-			'src': $this.attr('href'),
-			click: function(){
-				$(this).parent().trigger('click'); //fadeOut();
-			}
-		});
-		var xOvl = $('<div/>', {
-				'click': function(e){ $(this).remove()},
-				'id': 'simple-gallery-overly'
-		})
-		xOvl.css({
-// usually, one would use this: but this causes errors in IE, so please style this via css				
-					'backgroundColor': 'rgba(0,0,0,0.8)',
-//					'backgroundColor': '#000',
- 
-				'position': 'absolute',
-				'margin': '0px',
-				'padding': '0px',
-				'top': '0px',
-				'bottom': '0px',
-				'left': '0px',
-				'right': '0px',
-				'height': $(document).height() + 'px'
-		});
-		xOvl.appendTo( 'body' );
-		pic.appendTo( xOvl ).css({
-				'position': 'absolute',
-				'top': ( ( $(window).height()/2) - (pic.height() / 2) + $(document).scrollTop()  ) + "px",
-				'left': ( ( $(window).width()/2) - (pic.width() / 2)  ) + "px",
-				'border': '5px solid #9BBDDE',
-				'zIndex': '9999'
-			}).fadeIn('slow').css({
-				'top': ( ( $(window).height()/2) - (pic.height() / 2) + $(document).scrollTop()  ) + "px",
-				'left': ( ( $(window).width()/2) - (pic.width() / 2)  ) + "px",
+$(function(){
+	$("a[href$='png']:has(img), a[href$='jpg']:has(img), a[href$='gif']:has(img)").click(
+		function(e) {
+			$this = $(this);
+			var pic = $('<img />', {
+				'src': $this.attr('href'),
+				click: function(){
+					$(this).parent().trigger('click'); //fadeOut();
+				}
+			});
+			var xOvl = $('<div/>', {
+					'click': function(e){ $(this).remove()},
+					'id': 'simple-gallery-overly'
 			})
-	return false;
-	}
-)
+			xOvl.css({
+		// usually, one would use this: but this causes errors in IE, so please style this via css				
+						'backgroundColor': 'rgba(0,0,0,0.8)',
+		//					'backgroundColor': '#000',
+
+					'position': 'absolute',
+					'margin': '0px',
+					'padding': '0px',
+					'top': '0px',
+					'bottom': '0px',
+					'left': '0px',
+					'right': '0px',
+					'height': $(document).height() + 'px'
+			});
+			xOvl.appendTo( 'body' );
+			pic.appendTo( xOvl ).css({
+					'position': 'absolute',
+					'top': ( ( $(window).height()/2) - (pic.height() / 2) + $(document).scrollTop()  ) + "px",
+					'left': ( ( $(window).width()/2) - (pic.width() / 2)  ) + "px",
+					'border': '5px solid #9BBDDE',
+					'zIndex': '9999'
+				}).fadeIn('slow').css({
+					'top': ( ( $(window).height()/2) - (pic.height() / 2) + $(document).scrollTop()  ) + "px",
+					'left': ( ( $(window).width()/2) - (pic.width() / 2)  ) + "px",
+				})
+			return false; // prevent link click event
+		}
+	)
+});
