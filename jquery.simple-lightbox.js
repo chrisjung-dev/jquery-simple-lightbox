641904896b49c6ea8bcafb519f51a7eef512df36
Christian Jung <campino2k@gmail.com>
2010-07-15 19:51:24 +0200

    Move Static CSS to external Stylesheet
     - moved all static css styles to external css file
     - renamed overlay

--- jquery.simple-lightbox.js
+++ jquery.simple-lightbox.js
@@ -14,29 +14,15 @@ $(function(){
 			});
 			var xOvl = $('<div/>', {
 					'click': function(e){ $(this).remove()},
-					'id': 'simple-gallery-overly'
+					'id': 'simple-lightbox-overlay'
 			})
 			xOvl.css({
-		// usually, one would use this: but this causes errors in IE, so please style this via css				
-						'backgroundColor': 'rgba(0,0,0,0.8)',
-		//					'backgroundColor': '#000',
-
-					'position': 'absolute',
-					'margin': '0px',
-					'padding': '0px',
-					'top': '0px',
-					'bottom': '0px',
-					'left': '0px',
-					'right': '0px',
 					'height': $(document).height() + 'px'
 			});
 			xOvl.appendTo( 'body' );
 			pic.appendTo( xOvl ).css({
-					'position': 'absolute',
 					'top': ( ( $(window).height()/2) - (pic.height() / 2) + $(document).scrollTop()  ) + "px",
 					'left': ( ( $(window).width()/2) - (pic.width() / 2)  ) + "px",
-					'border': '5px solid #9BBDDE',
-					'zIndex': '9999'
 				}).fadeIn('slow').css({
 					'top': ( ( $(window).height()/2) - (pic.height() / 2) + $(document).scrollTop()  ) + "px",
 					'left': ( ( $(window).width()/2) - (pic.width() / 2)  ) + "px",
