007ebea7fd592280eb17872f5d560beeffa518c1
Christian Jung <campino2k@gmail.com>
2010-07-16 08:37:58 +0200

    Load Image before showing
     - append image in DOM via load-property
     - this ensures having properties when showing the pic

--- jquery.simple-lightbox.js
+++ jquery.simple-lightbox.js
@@ -6,27 +6,28 @@ $(function(){
 	$("a[href$='png']:has(img), a[href$='jpg']:has(img), a[href$='gif']:has(img)").click(
 		function(e) {
 			$this = $(this);
-			var pic = new Image();
-			$(pic).attr( src, $this.attr( 'href' ) );
-			$pic = $(pic);
-			$pic.click( function(){
-				$(this).parent().trigger('click');
+			$pic = $( '<img />', {
+				'src': $this.attr('href'),
+				'load': function(){
+					$(this).appendTo( xOvl ).css({
+						'top': ( ( $(window).height()/2) - ($pic.height() / 2) + $(document).scrollTop()  ) + "px",
+						'left': ( ( $(window).width()/2) - ($pic.width() / 2)  ) + "px"
+					}).fadeIn('slow')
+				},
+				'click': function(){
+					$(this).parent().trigger('click');
+				}
 			});
 			var xOvl = $('<div/>', {
-					'click': function(e){ $(this).remove()},
+					'click': function(e){ 
+						$(this).remove();
+					},
 					'id': 'simple-lightbox-overlay'
 			})
 			xOvl.css({
 					'height': $(document).height() + 'px'
 			});
 			xOvl.appendTo( 'body' );
-			pic.appendTo( xOvl ).css({
-					'top': ( ( $(window).height()/2) - (pic.height() / 2) + $(document).scrollTop()  ) + "px",
-					'left': ( ( $(window).width()/2) - (pic.width() / 2)  ) + "px",
-				}).fadeIn('slow').css({
-					'top': ( ( $(window).height()/2) - (pic.height() / 2) + $(document).scrollTop()  ) + "px",
-					'left': ( ( $(window).width()/2) - (pic.width() / 2)  ) + "px",
-				})
 			return false; // prevent link click event
 		}
 	)
