76ccc0caded86c0fc6cd88392b093a1fc374ed3f
Christian Jung <campino2k@gmail.com>
2010-07-17 15:43:35 +0200

    Turned Snippet into plugin
      - restrucured to meet plugin-format for jQuery
      - put some stuff in the demo
      - made fadingspeed a configurable option

--- jquery.simple-lightbox.js
+++ jquery.simple-lightbox.js
@@ -2,34 +2,41 @@
  * jquery.simple-lightbox
  * CC-BY-NC-SA Chris Jung <campino2k@gmail.com>
  */
-$(function(){
-	$("a[href$='png']:has(img), a[href$='jpg']:has(img), a[href$='gif']:has(img)").click(
-		function(e) {
-			$this = $(this);
-			$ovl = $('<div/>', {
-				'id': 'simple-lightbox-overlay',
-				'click': function(e){ 
-					$(this).remove();
-				},
-				css: ({
-					'height': $(document).height() + 'px'
+
+(function($) {
+	$.fn.simpleLightbox = function(options) {
+		options = $.extend( {}, $.fn.simpleLightbox.defaults, options );
+		return this.each( function(){
+			$(this).click( function() {
+				$this = $(this);
+				$ovl = $('<div/>', {
+					'id': 'simple-lightbox-overlay',
+					'click': function(e){ 
+						$(this).remove();
+					},
+					css: ({
+						'height': $(document).height() + 'px'
+					})
 				})
+				$ovl.appendTo( 'body' );
+				$pic = $( '<img />', {
+					'src': $this.attr('href'),
+					css: ({'display': 'none'}),
+					'load': function(){
+						$(this).appendTo( $ovl ).css({
+							'top': ( ( $(window).height()/2) - ($pic.height() / 2) + $(document).scrollTop()  ) + "px",
+							'left': ( ( $(window).width()/2) - ($pic.width() / 2)  ) + "px"
+						}).fadeIn( options.fadeSpeed  )
+					},
+					'click': function(){
+						$(this).parent().trigger('click');
+					}
+				});
+				return false; // prevent link click event
 			})
-			$ovl.appendTo( 'body' );
-			$pic = $( '<img />', {
-				'src': $this.attr('href'),
-				css: ({'display': 'none'}),
-				'load': function(){
-					$(this).appendTo( $ovl ).css({
-						'top': ( ( $(window).height()/2) - ($pic.height() / 2) + $(document).scrollTop()  ) + "px",
-						'left': ( ( $(window).width()/2) - ($pic.width() / 2)  ) + "px"
-					}).fadeIn('slow')
-				},
-				'click': function(){
-					$(this).parent().trigger('click');
-				}
-			});
-			return false; // prevent link click event
-		}
-	)
-});
+		})
+	},
+	$.fn.simpleLightbox.defaults = {
+		fadeSpeed: 'slow'
+	}
+})(jQuery);
